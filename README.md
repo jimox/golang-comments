6932dc141fc997536b21536bfd39c89dbd055318
Jim Oxenhandler <joxenhandler@fanatics.com>
2021-02-22 09:26:38 -0700

    Inital commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# golang-comments
+Adds simple Golang comments for the function, method or type of the line the cursor is on
+
+## Using
+In a Go file, put your cursor on the line of a function, method ortype. Invoke the Go: Add Comments extension witht eh keyboard shortcu (CMD+K C on Mac) (open the command palette (CMD + Shift + P on Mac) and look for the command 'Go: Add Comments'. Hit enter.)
+
+![install and work](images/demo.gif)
---
+++ src/extension.ts
@@ -0,0 +1,111 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "golang-comments" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('golang-comments.generateComments', () => {
+		
+		// Make sure there is an active editor
+		var editor = vscode.window.activeTextEditor;
+		if (editor) {
+			
+			// Make sure it's a Go file
+			var lang = editor.document.languageId;
+			if (lang === "go") {
+
+				// Get the text of the current line
+				const text = editor.document.lineAt(editor.selection.active.line).text.trim().replace(/\s+/g,' ');
+
+				// Get the line to add the comment to
+				var startLine = editor.selection.active.line - 1;
+
+				//  Find the name
+				let name = null;
+				
+				// First we need to know if we are dealing with a type or function/method
+				if (text.startsWith('type')) {
+					// type child struct {
+					name = text.split(' ')[1];
+				} else {					
+					// Now we need to know if this is a method or function
+					if ((text.match(/\(/g) || []).length > 1) {
+						// func (obj *parent) set(bar string) {
+						const start = text.indexOf(')') + 2;
+						const end = text.split('(', 2).join('(').length;
+						const len = end - start;
+						name = text.substr(start, len);
+					} else {
+						// func set(bar string) {
+						const start = 5;
+						const end = text.indexOf('(');
+						const len = end - start;
+						name = text.substr(start, len);
+					}
+				}
+
+				// If we have text to insert
+				if (name.length > 0) {
+
+					// Get the editor
+					let editor = vscode.window.activeTextEditor;
+					if (editor) {
+						var textToInsert = `// ${name} `;
+						editor.edit((editBuilder: vscode.TextEditorEdit) => {
+							if (startLine < 0) {
+								//If the function declaration is on the first line in the editor we need to set startLine to first line
+								//and then add an extra newline at the end of the text to insert
+								startLine = 0;
+								textToInsert += '\n';
+							}
+							
+							let editor = vscode.window.activeTextEditor;
+							if (editor) {
+									//Check if there is any text on startLine. If there is, add a new line at the end
+									var pos = new vscode.Position(startLine, 0);	
+									
+									// Get the preceding whitespace
+									const match = editor.document.lineAt(editor.selection.active.line).text.match(/(\s*)/g);
+
+									// Add it back in to the comment
+									if (match && match.length > 0) {
+										textToInsert = match[0] + textToInsert;
+									}
+									
+									// Add a newline
+									textToInsert = '\n' + textToInsert;
+
+									// Insert
+									editBuilder.insert(pos, textToInsert);
+
+									// Move the cursor to the end of the comment line
+									let range = editor.document.lineAt(pos).range;
+									editor.selection =  new vscode.Selection(range.start, range.end);
+									editor.revealRange(range);
+							}
+						}).then(() => {});
+
+						return;
+					}
+				}
+			}
+		} 
+
+		// Display an error message box to the user
+		//vscode.window.showInformationMessage('There was an error parsing your function.');
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
